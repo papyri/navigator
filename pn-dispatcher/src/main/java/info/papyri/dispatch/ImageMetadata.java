50605d7474df4c297ed6f21a35941dbba7a7f80a
hcayless <hcayless@71858626-4ce2-415c-9a67-6d0363ee0835>
2010-09-14 18:29:35 +0000

    Added servlet to return Djatoka image metadata.

---
+++ pn-dispatcher/src/main/java/info/papyri/dispatch/ImageMetadata.java
@@ -0,0 +1,83 @@
+package info.papyri.dispatch;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hcayless
+ */
+public class ImageMetadata extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/plain;charset=UTF-8");
+        OutputStream out = response.getOutputStream();
+        try {
+          URL url = new URL("http://dl-img.home.nyu.edu/adore-djatoka/resolver?url_ver=Z39.88-2004&rft_id="
+                  + request.getParameter("url")
+                  + "&svc_id=info:lanl-repo/svc/getMetadata");
+          InputStream in = url.openStream();
+          byte[] b = new byte[1024];
+          int read = 0;
+          while ((read = in.read(b)) > 0) {
+            out.write(b, 0, read);
+          }
+          in.close();
+        } catch (Exception e) {
+          out.write("{}".getBytes());
+        } finally {
+          out.close();
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
