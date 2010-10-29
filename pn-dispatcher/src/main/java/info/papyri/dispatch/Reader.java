c28c4d96d9ff4910c51a94630356435b0fdebb40
hcayless <hcayless@71858626-4ce2-415c-9a67-6d0363ee0835>
2010-10-29 15:42:45 +0000

    Testing.

--- pn-dispatcher/src/main/java/info/papyri/dispatch/Reader.java
+++ pn-dispatcher/src/main/java/info/papyri/dispatch/Reader.java
@@ -52,7 +52,7 @@ public class Reader extends HttpServlet {
     if (page != null) {
       // Redirection for old static URLs
       System.out.println(page);
-      if (page.contains("idp_static") && (page.contains("-citations-") || page.contains("index.html"))) {
+      if (page.contains("current") && (page.contains("-citations-") || page.contains("index.html"))) {
         response.sendError(HttpServletResponse.SC_GONE);
       } else if (page.endsWith(".html")) {
         if (page.contains("ddb/html") || page.contains("aggregated/html")) {
