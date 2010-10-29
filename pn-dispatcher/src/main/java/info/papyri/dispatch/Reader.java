f603fb2739df219a5a5bb3286dd59ed0b610e1e2
hcayless <hcayless@71858626-4ce2-415c-9a67-6d0363ee0835>
2010-10-29 15:22:58 +0000

    Sending a GONE for idp_static pages.

--- pn-dispatcher/src/main/java/info/papyri/dispatch/Reader.java
+++ pn-dispatcher/src/main/java/info/papyri/dispatch/Reader.java
@@ -59,8 +59,7 @@ public class Reader extends HttpServlet {
           response.setHeader("Location", page.replaceAll(".*/HGV\\d/([^.]+).html", "http://papyri.info/hgv/$1"));
           response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
         }
-      } else if (page.contains("idp_static")) {
-        response.setHeader("Location", "/");
+      } else if (page.contains("-citations-") || page.endsWith("index.html")) {
         response.setStatus(HttpServletResponse.SC_GONE);
       }
       else if (page.contains("/")) {
