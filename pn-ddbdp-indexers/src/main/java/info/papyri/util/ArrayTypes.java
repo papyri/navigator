ee16e1c73069b6e725c735b757cc0ad9cc653a2f
hcayless <hcayless@71858626-4ce2-415c-9a67-6d0363ee0835>
2010-11-30 15:59:36 +0000

    Removing old PN code.

--- pn-ddbdp-indexers/src/main/java/info/papyri/util/ArrayTypes.java
+++
@@ -1,6 +0,0 @@
-package info.papyri.util;
-import org.apache.lucene.index.Term;
-public abstract class ArrayTypes {
-    public static final String [] STRING = new String[0];
-    public static final Term [] TERM = new Term[0];
-}
--- pn-ddbdp-indexers/src/main/java/org/apache/lucene/index/TermTextSwap.java
+++
@@ -1,8 +0,0 @@
-package org.apache.lucene.index;
-
-public abstract class TermTextSwap {
-    public static Term swapText(Term t, String newText){
-        t.text = newText;
-        return t;
-    }
-}
