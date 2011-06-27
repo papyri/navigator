3679a45cbb7fbc609b94c9f2a178295b63ccc087
t-hill <thill@nyu.edu>
2011-06-27 21:54:54 +0100

    HTML and custom exception class for faceted browse.

---
+++ pn-dispatcher/src/main/java/info/papyri/dispatch/browse/FieldNotFoundException.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.papyri.dispatch.browse;
+
+/**
+ *
+ * @author thill
+ */
+public class FieldNotFoundException extends Exception{
+    
+    private String info;
+    private String field;
+    
+    public FieldNotFoundException(){
+        
+        super();
+        field = "Unknown";
+        info = "Unknown";
+        
+    }
+    
+    public FieldNotFoundException(String field){
+        
+        super();
+        this.field = field;
+        info = "Unknown";
+        
+    }
+    
+    public FieldNotFoundException(String field, String info){
+        
+        super();
+        this.field = field;
+        this.info = info;
+        
+    }
+    
+    public String getError(){
+        
+        return "Field " + field + " missing with values " + info;
+        
+    }
+    
+}
