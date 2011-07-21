826a742427eb7490fee78a3fb69203d4f3b7cea7
t-hill <thill@nyu.edu>
2011-07-21 14:59:10 +0100

    Adding "no translation" value to translation facet.

--- pn-dispatcher/src/main/java/info/papyri/dispatch/browse/facet/TranslationFacet.java
+++ pn-dispatcher/src/main/java/info/papyri/dispatch/browse/facet/TranslationFacet.java
@@ -2,6 +2,13 @@ package info.papyri.dispatch.browse.facet;
 
 import info.papyri.dispatch.LanguageCode;
 import info.papyri.dispatch.browse.SolrField;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.FacetField.Count;
+import org.apache.solr.client.solrj.response.QueryResponse;
 
 /**
  *
@@ -46,7 +53,70 @@ public class TranslationFacet extends Facet {
         return displayValue;
         
     }
+    
+    @Override
+    public SolrQuery buildQueryContribution(SolrQuery solrQuery){
+        
+        solrQuery.addFacetField(field.name());
+        solrQuery.setFacetLimit(-1);
+        
+        Iterator<String> cit = facetConstraints.iterator();
+        
+        while(cit.hasNext()){
+            
+            String fq = cit.next();
+            if(fq.equals("No translation")){
+                
+                fq = "-" + field.name() + ":[* TO *]";
+                
+            }
+            else{
+                
+                // slash-escape madness: java, solr, and java.regex all use backslash
+                // as an escape character
+                fq = fq.replaceAll("\\\\", "\\\\\\\\");
+                if(fq.contains(" ")) fq = "\"" + fq + "\"";
+                fq = field.name() + ":" + fq;
+            
+            }
+            
+            solrQuery.addFilterQuery(fq);
 
+            
+        }
+        
+        return solrQuery;
+        
+    }
+    
+
+    public void setWidgetValues(QueryResponse queryResponse){
+        
+        FacetField facetField = queryResponse.getFacetField(field.name());
+        valuesAndCounts = new ArrayList<Count>();
+        List<Count> unfiltered = facetField.getValues();
+        Iterator<Count> cit = unfiltered.iterator();
+        long total = 0;
+        while(cit.hasNext()){
+            
+            Count count = cit.next();            
+            if(count.getName() != null && !count.getName().equals("") && count.getCount() > 0 && !count.getName().equals("null")){
+                
+                valuesAndCounts.add(count);
+                total += count.getCount();
+            }
+            
+        }
+        
+        long noTranslationTotal = queryResponse.getResults().getNumFound() - total;
+        
+        if(noTranslationTotal > 0){
+            
+            valuesAndCounts.add(new Count(new FacetField(SolrField.translation_language.name()), "No translation", noTranslationTotal));
+         
+        }
+    }
+    
     @Override
     String getToolTipText() {
         
