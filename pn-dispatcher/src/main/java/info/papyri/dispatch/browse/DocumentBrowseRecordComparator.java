cf8e2c3ec9b878ba43db54839d3649dc0906a237
t-hill <thill@nyu.edu>
2011-08-01 16:01:46 +0100

    Comparator working for finding preferred identifier

---
+++ pn-dispatcher/src/main/java/info/papyri/dispatch/browse/DocumentBrowseRecordComparator.java
@@ -0,0 +1,92 @@
+package info.papyri.dispatch.browse;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author thill
+ */
+public class DocumentBrowseRecordComparator implements Comparator {
+
+    @Override
+    public int compare(Object t, Object t1) {
+        
+        String id1 = (String) t;
+        String id2 = (String) t1;
+        
+        ArrayList<String> ids1 = splitIntoNumericAndAlphabeticComponents(id1);
+        ArrayList<String> ids2 = splitIntoNumericAndAlphabeticComponents(id2);
+        
+        Pattern letterCheck = Pattern.compile("^\\D.*");
+        Pattern numberCheck = Pattern.compile("^\\d.*");
+  
+        
+        // loop through all components
+        
+        // make sure no index errors possible by choosing shorter of two arrays
+        
+        int limit = ids1.size() < ids2.size() ? ids1.size() : ids2.size();
+
+        for(int i = 0; i < limit; i++){
+            
+            String id1chunk = ids1.get(i);
+            String id2chunk = ids2.get(i);
+            
+            // if the two are of different types, return numerical types first
+            
+            if(numberCheck.matcher(id1chunk).matches() && letterCheck.matcher(id2chunk).matches()) return -1;
+            if(letterCheck.matcher(id1chunk).matches() && numberCheck.matcher(id2chunk).matches()) return 1;
+
+            // if they're numerical, do numerical sort
+            
+            if(numberCheck.matcher(id1chunk).matches()){
+
+                if(Integer.valueOf(id1chunk) < Integer.valueOf(id2chunk)) return -1;
+                if(Integer.valueOf(id1chunk) > Integer.valueOf(id2chunk)) return 1;
+                
+            }
+            else{
+
+                int compareValue = id1chunk.compareToIgnoreCase(id2);
+                if(compareValue != 0) return compareValue;
+                
+            }
+            
+            
+        }
+        
+        if(ids1.size() < ids2.size()) return -1;
+        if(ids1.size() > ids2.size()) return 1;
+        
+        return 0;
+        
+    }
+    
+    ArrayList<String> splitIntoNumericAndAlphabeticComponents(String mixedString){
+        
+        ArrayList<String> components = new ArrayList<String>();
+        Pattern findFirstAlphabetical = Pattern.compile("([\\D]+)");
+        int findIndex = 0;
+        
+        Matcher matcher = findFirstAlphabetical.matcher(mixedString);
+        
+        while(matcher.find()){
+            
+            String numbers = mixedString.substring(findIndex, matcher.start());
+            if(!"".equals(numbers)) components.add(numbers); 
+            String letters = matcher.group(1);
+            components.add(letters);
+            findIndex = matcher.end();
+            
+        }
+         
+        if(findIndex < mixedString.length()) components.add(mixedString.substring(findIndex));
+        
+        return components;
+        
+    }
+    
+}
