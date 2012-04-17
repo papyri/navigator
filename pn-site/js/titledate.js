a2e33af01c68ff990bb4020e2343518153f67ad9
t-hill <thill@nyu.edu>
2012-04-17 16:02:38 +0100

    Fixed history mouseover/click behaviour

--- pn-site/js/titledate.js
+++ pn-site/js/titledate.js
@@ -49,26 +49,42 @@ $(document).ready(function(){
             }
             return result;
           });
+    
+    	
+    	  var showEditHistory = function(){
+    	  
+    	      	hideAllHistory();
+    	  		$("#edit-history-list").show("blind", 250); 
+    	  		$("#edit-history").unbind("click");
+    	  		$("#edit-history").bind("click", hideEditHistory);
+    	  
+    	  }
 
-          $("#edit-history").mouseover( function(){
-          
-          	$("#edit-history-list").show("blind", 250);
-          
-          });
-          $("#edit-history").mouseleave( function(){
-          
-          	$("#edit-history-list").hide("blind", 250);
-          
-          });
-          $("#all-history").mouseover( function(){
-          
-          	$("#all-history-list").show("blind", 250);
-          
-          });
-          $("#all-history").mouseleave( function(){
-          
-          	$("#all-history-list").hide("blind", 250);
-          
-          });
+		  var hideEditHistory = function(){
+			
+          		if(!($("#edit-history-list").css("display") == "none"))  {$("#edit-history-list").hide("blind", 250); }
+    	  		$("#edit-history").unbind("click");
+    	  		$("#edit-history").bind("click", showEditHistory);			
+		   }
+
+		  var showAllHistory = function(){
+
+    	  		hideEditHistory();
+    	  		$("#all-history-list").show("blind", 250); 
+    	  		$("#all-history").unbind("click");
+    	  		$("#all-history").bind("click", hideAllHistory);
+		  
+		  }
+		  
+		  var hideAllHistory = function(){
+		  
+    	  		if(!($("#all-history-list").css("display") == "none"))  { $("#all-history-list").hide("blind", 250); }
+    	  		$("#all-history").unbind("click");
+    	  		$("#all-history").bind("click", showAllHistory);		  
+		  
+		  }
+
+          $("#edit-history").bind("click", showEditHistory);
+          $("#all-history").bind("click", showAllHistory);
 
 });
