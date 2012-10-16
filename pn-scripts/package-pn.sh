298fbac37d24ca78c5b9f35f4240d87a98975a75
Hugh Cayless <hugh.cayless@nyu.edu>
2012-10-16 15:25:24 -0400

    Updated PN packaging code.

--- pn-scripts/package-pn.sh
+++ pn-scripts/package-pn.sh
@@ -9,9 +9,12 @@ mkdir $DATE/text
 mkdir $DATE/rdf
 
 git clone /data/papyri.info/idp.data "$DATE/xml"
-rsync -av --include "*.html" --exclude "*.txt" /data/papyri.info/pn/idp.html/ "$DATE/html"
-rsync -av --include "*.txt" --exclude "*.html" /data/papyri.info/pn/idp.html/ "$DATE/text"
+rsync -v --include "*.html" --exclude "*.txt" /data/papyri.info/pn/idp.html/ "$DATE/html"
+rsync -v --include "*.txt" --exclude "*.html" /data/papyri.info/pn/idp.html/ "$DATE/text"
+rsync -a /data/papyri.info/pn/home "$DATA/html/"
 QUERY="CONSTRUCT%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D%0AFROM%20%3Crmi%3A%2F%2Flocalhost%2Fpapyri.info%23pi%3E%0AWHERE%20%7B%20%3Fs%20%3Fp%20%3Fo%20%7D"
-curl "http://localhost/mulgara/sparql/?query=$QUERY&format=n3" > "$DATE/rdf/papyri.info.n3"
+curl -H "Accept: text/plain" -H "Accept-Charset: utf-8" "http://localhost/sparql/?query=$QUERY&output=n3" > "$DATE/rdf/papyri.info.n3"
 
+rm -rf *.zip
 zip -r "$DATE.zip" $DATE
+rm -rf $DATE
