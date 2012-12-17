ae4a45c21489ca6e2a141885a11960c2de097f5e
Hugh Cayless <philomousos@gmail.com>
2012-12-17 14:39:04 -0500

    Getting the tests to run.

--- pn-mapping/project.clj
+++ pn-mapping/project.clj
@@ -3,15 +3,16 @@
   :repositories {"papyri.info" "http://dev.papyri.info/maven"
                  "apache" "https://repository.apache.org/content/repositories/releases/"
                  "apache-snapshots" "https://repository.apache.org/content/repositories/snapshots"}
+  :jvm-opts ["-Xmx1g"]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [net.sf.saxon/saxon-he "9.2.0.2"]
                  [xerces/xercesImpl "2.9.1"]
                  [xml-apis/xml-apis "1.3.04"]
                  [xml-resolver/xml-resolver "1.2"]
-                 [org.apache.jena/jena-core "2.7.1-SNAPSHOT"]
-                 [org.apache.jena/jena-iri "0.9.1-SNAPSHOT"]
-                 [org.apache.jena/jena-arq "2.9.1-SNAPSHOT"]
-                 [org.apache.jena/jena-fuseki "0.2.2-SNAPSHOT"]
+                 [org.apache.jena/jena-core "2.7.1"]
+                 [org.apache.jena/jena-iri "0.9.1"]
+                 [org.apache.jena/jena-arq "2.9.1"]
+                 [org.apache.jena/jena-fuseki "0.2.2"]
                  [commons-logging/commons-logging "1.1.1"]]
   :main info.papyri.map)
