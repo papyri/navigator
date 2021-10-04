4fd7fcf2972b8eae186a7b963a21c4660bfbc42a
Hugh Cayless <philomousos@gmail.com>
2021-10-04 09:43:47 -0400

    Don't log.

--- pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
+++ pn-sync/src/main/java/info/papyri/sync/GitWrapper.java
@@ -91,7 +91,6 @@ public class GitWrapper {
   @SuppressWarnings({"null"})
   public static String getPreviousSync() throws Exception {
     String result = null;
-    Class.forName("org.postgresql.Driver");
     try (Connection connect = DriverManager.getConnection(
             "jdbc:postgresql://localhost/pn?"
                     + "user=" + git.dbUser + "&password=" + git.dbPass)) {
